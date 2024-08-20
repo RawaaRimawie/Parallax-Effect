6eea119b9b038521e31c376e99d6d7417eff8e85
Rawarimaei <125211778+Rawarimaei@users.noreply.github.com>
2024-08-20 16:29:28 +0300

    first commit

---
+++ javaS.js
@@ -0,0 +1,17 @@
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageYOffset;
+
+    const parallax1 = document.querySelector('.parallax-1');
+    const parallax2 = document.querySelector('.parallax-2');
+    const parallax3 = document.querySelector('.parallax-3');
+
+    parallax1.style.transform = 'translateY(' + (scrollTop * 0.3) + 'px)';
+    parallax2.style.transform = 'translateY(' + (scrollTop * 0.4) + 'px)';
+    parallax3.style.transform = 'translateY(' + (scrollTop * 0.9) + 'px)';
+    
+});
+
+
+
+
+
